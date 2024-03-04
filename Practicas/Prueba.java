6baa8f813b0c4663d779782b025a015502e29169
sergiolopez123 <159494442+sergiolopez123@users.noreply.github.com>
2024-03-04 15:52:06 -0700

    Add files via upload

---
+++ Practicas/Prueba.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Prueba {
+public static void main (String args[]){
+Scanner Scan = new Scanner(System.in);
+System.out.println ("Introduce tu nombre");
+String nombre = Scan.nextLine();
+System.out.println(nombre);
+}
+
+}
