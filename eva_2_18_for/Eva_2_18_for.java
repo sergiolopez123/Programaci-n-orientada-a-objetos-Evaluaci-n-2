8c8491a33e92430a740571469bdf6e59872b24f1
sergiolopez123 <159494442+sergiolopez123@users.noreply.github.com>
2024-04-09 16:51:23 -0600

    Add files via upload

---
+++ eva_2_18_for/Eva_2_18_for.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_18_for;
+
+/**
+ *
+ * @author jairl
+ */
+public class Eva_2_18_for {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //inicio; condicion; incremento/decremento
+        for(int i = 1; i <= 10; i++){
+            //llaves son opcionales, usenlas cuando tengan mas de una instruccion a repetir
+            System.out.print(i+"-");
+        }System.out.println("");
+        for(int i = 10; i >= 1; i--){
+            //llaves son opcionales, usenlas cuando tengan mas de una instruccion a repetir
+            System.out.print(i+"-");}
+        System.out.println(""); 
+for(int i = 0; i <= 100; i+=2){
+    System.out.print(i + "-");
+        //imprimir todos los numeros pares entre 0 y 100
+    } 
+}    
+    
+}
